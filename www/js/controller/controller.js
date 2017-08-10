c54997411fb75b9663add6b002c4f93199f867c3
oscar-cortez <od_cortez@galileo.edu>
2017-08-10 15:03:07 -0600

    add search autocomplete table

--- www/js/controller/controller.js
+++ www/js/controller/controller.js
@@ -1,5 +1,26 @@
-app.controller("searchCtrl", function($scope){
+var config = {
+    apiKey: "AIzaSyDTvoq6e7fckj5fjE2dE8OGCx4XuKOSHU8",
+    authDomain: "cmi-db-33e5d.firebaseapp.com",
+    databaseURL: "https://cmi-db-33e5d.firebaseio.com",
+    projectId: "cmi-db-33e5d",
+    storageBucket: "cmi-db-33e5d.appspot.com",
+    messagingSenderId: "970565718586"
+  };
+var CMI = firebase.initializeApp(config);
+app.controller("searchCtrl", function($scope, $firebaseArray){
+	var ref = CMI.database().ref('Invitados');
+  $scope.objeInvitados = $firebaseArray(ref);
+	$scope.objeInvitados.$loaded(function() {
+		console.info($scope.objeInvitados);
+	})
   $scope.clickInput = function(){
     $scope.classInput = "inputSearchYourNameTop"
   }
+
+  $scope.selectItem = function(item) {
+		console.log(item)
+
+	}
+
+
 })
